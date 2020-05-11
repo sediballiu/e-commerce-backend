caa7006a5ff1e32ba24bbb8b7b2344413527350b
sedi <sedi@sedi.sedi.com>
2020-05-11 20:27:53 +0200

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 const port = process.env.PORT || 4000;
 
 const { sequelize } = require('./db');
 app.use(express.json());
+app.use(cors());
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
